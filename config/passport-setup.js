566d8926e529a7f8d62ed59da0513b231b84009b
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-02-04 14:36:15 +0530

    Securing keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,13 +1,12 @@
 const passport = require("passport");
 const GoogleStratergy = require("passport-google-oauth2");
-
+const keys = require("./keys");
 passport.use(
   new GoogleStratergy(
     {
       // options for the google stratergy
-      clientID:
-        "245526895964-upj0mdim2c6g1q1juahhkp0m7g5iinsm.apps.googleusercontent.com",
-      clientSecret: "-0qUMMjqx3IoQuu95f8bqcoz"
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret
     },
     () => {
       // passport callback function
